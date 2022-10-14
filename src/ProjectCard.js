99ff2576c33d9a55136ed15e2f8deaa9ecc5de58
Lisa <lisamichellemurray@gmail.com>
2022-10-13 21:52:59 -0500

    fixed project card hover

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -4,16 +4,18 @@ import "./stylesheets/ProjectCard.css";
 
 
 function ProjectCard({ title, siteLink, description, backgroundLink}) {
-    return(
-    <div className="card-wrapper">
+    return(<div>
         <Link to={`${siteLink}`}>
-            <div className="card-background"  style={{ backgroundImage: `url(${backgroundLink})` }}>
-                {/* <div className="card-title text">{title}</div> */}
-            </div>
-            <div className="card-overlay">
-                <div className="card-description text">{description}</div>
-            </div>
-        </Link> 
+                <div className="card-container">
+                    <img src={backgroundLink} alt={title} className="image"/>
+                    <div className="title">
+                        <div className="text">{title}</div>
+                    </div>
+                    <div className="description">
+                        <div className="text">{description}</div>
+                    </div>
+                </div>
+        </Link>
         
     </div>)
 }
