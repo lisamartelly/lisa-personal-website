146ac8980c7773cb7d4d374aa1b6e1a23fb5a25e
Lisa <lisamichellemurray@gmail.com>
2022-10-20 11:57:20 -0500

    cleaning up code

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import { render } from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { StrictMode } from 'react';
 import Homepage from './Homepage';
-import ReturnResults from './ReturnResults';
-import TransitWeather from './TransitWeather';
-import FullMoonFanClub from './FullMoonFanClub';
 import Footer from './Footer';
 
 import "./stylesheets/App.css";
@@ -20,9 +17,6 @@ const App = () => {
           <Routes >
             {/* <Route path="/project/:id" element={<Project />} /> */}
             <Route path="/" element={<Homepage />}/>
-            <Route path="/returning-research-results" element={<ReturnResults />}/>
-            <Route path="/transit-weather-app" element={<TransitWeather />}/>
-            <Route path="/full-moon-fan-club" element={<FullMoonFanClub />}/>
           </Routes>
           < Footer />
         </BrowserRouter>
